4325ef73c552a55d8c42ebede07dce49d1364353
kris10ansn <kristian.nessa@protonmail.com>
2022-07-04 09:20:49 +0200

    use get_custom_field instead of the_field

--- template-parts/content.php
+++ template-parts/content.php
@@ -8,13 +8,14 @@
  * @package coolmat
  */
 
+require_once __DIR__ . "/../util/get_custom_field.php";
 
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class("entry"); ?>>
 	<header>
 		<h1><?php the_title() ?></h1>
-		<div class="menu-number"><?php the_field("menu_item_number") ?></div>
+		<div class="menu-number"><?php get_custom_field("menu_item_number") ?></div>
 		<div class="price"><?php the_content() ?></div>
 	</header>
 
